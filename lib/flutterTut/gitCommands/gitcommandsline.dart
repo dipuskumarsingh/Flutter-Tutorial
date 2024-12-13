9602a6cbe3a2d496d6a262bd3ef05d8d390b2e52
Dipu Kumar <dipukumarsingh7810@gmail.com>
2024-12-13 16:10:42 +0530

    githum command lines add

---
+++ lib/flutterTut/gitCommands/gitcommandsline.dart
@@ -0,0 +1,10 @@
+
+
+
+/// To create branch in git ==> git checkout -b NewBranch (-b is stand for branch name.
+/// To change the branch in git ==> git checkout main ( it is used for change the branch).
+/// To add file/folder in git ==> git  add . ( its add all the file/folder in git).
+/// To check status in git ==> git status. (its show the status of file/folder)
+/// To commit the files or folder ==> git commit -m "Here write the massege what you want....." (-m is stand for massage).
+/// To push the files or floder ==> git push origin (write here brach name..) , (it push the files in branch where you pused)
+/// To know the branch which branch you have ==> git branch (it show the current brach)
